ce974fc03eb1cce4446e2cf37e7346f43cc4a595
aldia_mechdream <futami16237@gmail.com>
2026-06-16 15:27:32 +0800

    因为node_modules的存在会触发检查，但其实不需要检查。所以通过临时改名法解决

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,11 +1,35 @@
 import { execSync } from 'node:child_process';
-import { mkdirSync, rmSync } from 'node:fs';
+import { existsSync, mkdirSync, renameSync, rmSync } from 'node:fs';
 import { join } from 'node:path';
 
 import { getPackageInfo } from './common/package-info.js';
 import { build } from './build.js';
 
 export function publish(who: string | undefined) {
+  const info = getPackageInfo(who);
+  build(who);
+
+  const binPath = join(info.path, 'bin');
+  mkdirSync(binPath, { recursive: true });
+
+  const nm0 = join(info.path, 'node_modules');
+  const nm1 = join(info.path, 'hidden_node_modules');
+  if (existsSync(nm0)) {
+    renameSync(nm0, nm1);
+  }
+
+  const ignoreFile = join(import.meta.dirname, '..', 'configs', '.vscodeignore');
+  execSync(`vsce package --ignoreFile ${ignoreFile} -o ${binPath}`, {
+    stdio: 'inherit',
+    cwd: info.path,
+  });
+
+  if (existsSync(nm1)) {
+    renameSync(nm1, nm0);
+  }
+}
+
+export function publish2(who: string | undefined) {
   const info = getPackageInfo(who);
   build(who);
 
